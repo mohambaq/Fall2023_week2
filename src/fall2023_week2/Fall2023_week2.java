0ed5a2cce99b49f094b18581ca70c8c5bd45f1d2
mohambaq <mohambaq@sheridancollege.ca>
2023-09-18 10:59:51 -0400

    Fall2023_week2

---
+++ src/fall2023_week2/Fall2023_week2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fall2023_week2;
+
+/**
+ *
+ * @author Mohammed Baqar
+ */
+public class Fall2023_week2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Mohammed Baqar");
+    }
+    
+}
